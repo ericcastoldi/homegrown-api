afcb63335d207a360aa82913419347107527ac50
Eric Castoldi <ericgtr17@gmail.com>
2017-10-03 10:04:52 -0300

    Implementação de uma POC inicial de consumo de planilhas do Google Spreadsheets

---
+++ poc.js
@@ -0,0 +1,24 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const GoogleSpreadsheet = require('google-spreadsheet');
+const credentials = require('./client-secret.json');
+
+const sheet = new GoogleSpreadsheet('1DxzIdHkuZPZq6Q_5etViHlEZIUPQJkgPqs8Wc49GfRg');
+
+sheet.useServiceAccountAuth(credentials, (authErr) => {
+
+    if(authErr) {
+        console.error(authErr);
+        return;
+    }
+
+    sheet.getRows(1, (err, rows) => {
+        if(err){
+            console.error(authErr);
+            return;
+        }
+            
+       console.log(rows); 
+    });
+   
+});
