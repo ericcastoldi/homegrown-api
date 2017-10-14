befe2cd3de185ffd63d69283398646e935504437
Eric Castoldi <ericgtr17@gmail.com>
2017-10-14 18:23:39 -0300

    Remoção da implementação de leitura de cupons fiscais (será implementada em um branch a parte)

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// const spreadsheetService = require('./services/spreadsheet-service');
-// const consolePrinter = require('./printers/consolePrinter.js');
-// const discardRaw = require('./filters/discardRaw.js');
-//
-// spreadsheetService.getItems()
-//     .then(discardRaw)
-//     .then(consolePrinter)
-//     .catch(console.error);
-//
+const spreadsheetService = require('./services/spreadsheet-service');
+const consolePrinter = require('./printers/consolePrinter.js');
+const discardRaw = require('./filters/discardRaw.js');
 
-const receiptFile = './parsers/receipt-parser/test-receipts/cervejas-x300.jpeg';
-const receiptProvider = require('./parsers/receipt-parser');
-receiptProvider.getReceipt(receiptFile)
-    .then(result => { console.log(result.text); process.exit(0); } )
+spreadsheetService.getItems()
+    .then(discardRaw)
+    .then(consolePrinter)
     .catch(console.error);
+
+
+
